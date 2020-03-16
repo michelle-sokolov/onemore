ccd5b72af74cf231ba0a276a00ed6f7c6a709fa2
Michelle Sokolov <michellesokolov@Michelles-MacBook-Pro.local>
2020-03-16 16:26:21 -0700

    update

--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -1,16 +1,36 @@
 import React, { Component } from "react";
-import BlogForm from "../components/blog";
+import axios from "axios";
 
-class Blog extends Component {
+import ArticlePreview from "../components/articlePreview";
+
+export default class Blog extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+  componentDidMount() {
+    axios
+      .get(
+        "https://public-api.wordpress.com/rest/v1/sites/ravensrestoration.wordpress.com/posts"
+      )
+      .then(res => {
+        console.log(res.data.posts);
+        this.setState({ posts: res.data.posts });
+      })
+      .catch(error => console.log(error));
+  }
+
   render() {
     return (
-      <div>
-        <center>
-          <BlogForm />
-        </center>
+      <div className="blog">
+        <div className="anchor" id="blog" />
+        <h1 className="sectionTitle">Articles</h1>
+        {this.state.posts.map(post => (
+          <ArticlePreview post={post} />
+        ))}
       </div>
     );
   }
 }
-
-export default Blog;

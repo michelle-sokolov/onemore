b2952270fbbced87f2d8ed80ddcf96d09ebcfa3a
Michelle Sokolov <michellersokolov@gmail.com>
2020-03-17 13:33:33 -0700

    Update Detail.js

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -16,7 +16,7 @@ class Detail extends Component {
         </center>
         <center>
           <h5>
-            Our product is made to order. Once your ready to make a purchase,
+            Our product is made to order. Once you are ready to make a purchase,
             <a href="/contact">contact us here</a> and we'll get the process
             started.
           </h5>

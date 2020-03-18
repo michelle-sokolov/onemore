46278fca152c37b15ea163e813b4e5b8c2113f4f
DN <dilaran@live.com>
2020-03-18 13:36:16 -0700

    carousel

--- client/src/components/Carousel/DemoCarousel.js
+++ client/src/components/Carousel/DemoCarousel.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
- 
+import Img from 'react-image';
 class DemoCarousel extends Component {
     render() {
         return (
             <Carousel>
                 <div>
-                    <img src="../assets/1.jpeg" />
+                    <Img src="../assets/photo.jpeg" />
                     <p className="legend">Legend 1</p>
                 </div>
                 <div>
-                    <img src="../assets/2.jpg" />
+                    <img src="photo.img" />
+                    {/* <img src="../assets/2.jpg" /> */}
                     <p className="legend">Legend 2</p>
                 </div>
                 <div>

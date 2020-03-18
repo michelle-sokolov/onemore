87b57dbc025080ce745912ed47a8ffb59c55b097
DN <dilaran@live.com>
2020-03-18 16:46:39 -0700

    deleting and stuff

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,45 +1,45 @@
 import React from "react";
 import "./style.css";
 
-export default class Layout extends React.Component {
-  render() {
-    return (
-      <div className="about">
-        <div className="anchor" id="about" />
-        <h1 className="sectionTitle">About Us</h1>
-        <div className="twocols">
-          <div className="aboutText">
-            <h3>
-             WOODWORKSCOMPANY
-            </h3>
-            <h2>
+// export default class Layout extends React.Component {
+//   render() {
+//     return (
+//       <div className="about">
+//         <div className="anchor" id="about" />
+//         <h1 className="sectionTitle">About Us</h1>
+//         <div className="twocols">
+//           <div className="aboutText">
+//             <h3>
+//              WOODWORKSCOMPANY
+//             </h3>
+//             <h2>
             
-            </h2>
-          </div>
-          <div id="porfileimg">
-            <img
-              className="circle"
-              alt="Woodworks"
-              src="https://www.livingspaces.com/inspiration/styles/modern/dining-room-macie-black-dining-table"
-            />
-          </div>
-        </div>
-        <div className="Skills">
-          <h3>whatever we good in</h3>
-          <div className="twocols">
-            <div className="projs">
-              <ul>
+//             </h2>
+//           </div>
+//           <div id="porfileimg">
+//             <img
+//               className="circle"
+//               alt="Woodworks"
+//               src="https://www.livingspaces.com/inspiration/styles/modern/dining-room-macie-black-dining-table"
+//             />
+//           </div>
+//         </div>
+//         <div className="Skills">
+//           <h3>whatever we good in</h3>
+//           <div className="twocols">
+//             <div className="projs">
+//               <ul>
                 
-              </ul>
-            </div>
-            <div className="projs">
-              <ul>
+//               </ul>
+//             </div>
+//             <div className="projs">
+//               <ul>
                 
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+//               </ul>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import HomeItem from "../components/home";
-import DemoCarousel from "../components/Carousel/DemoCarousel";
-import About from "../components/About"
+// import DemoCarousel from "../components/Carousel/DemoCarousel";
+// import About from "../components/About"
 class Home extends Component {
   render() {
     return (
       <div>
         <center>
           <HomeItem />
-         <DemoCarousel />
-         <About />
+         {/* <DemoCarousel /> */}
+         {/* <About /> */}
         </center>
       </div>
     );

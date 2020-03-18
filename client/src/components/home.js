4a84e9012613cf54eea363e8e86c8e3a9a0e05e3
DN <dilaran@live.com>
2020-03-17 18:32:18 -0700

    carousel with no images

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+
 function HomeItem() {
   return (
     <div>
       <br />
       <br />
       <h1>HOME PAGE STUFF GOES HERE</h1>
+ 
     </div>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import HomeItem from "../components/home";
-
+import DemoCarousel from "../components/Carousel/DemoCarousel";
 class Home extends Component {
   render() {
     return (
       <div>
         <center>
           <HomeItem />
+         <DemoCarousel />
         </center>
       </div>
     );

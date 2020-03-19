038bc35b0c2382dffe5f37e08704d7d945165942
Michelle Sokolov <michellesokolov@Michelles-MacBook-Pro.local>
2020-03-18 20:13:59 -0700

    changes

--- client/src/components/wishlist.js
+++ client/src/components/wishlist.js
@@ -1,4 +1,4 @@
-// import React, { Component } from "react";
+import React, { Component } from "react";
 // import values from "../items.json";
 
 // export default class WishListItem extends Component {
